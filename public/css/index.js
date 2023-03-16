ff7d099cb8156b522bb3484cd846cce802e5ca6b
devin cet <devincetin7@gmail.com>
2023-03-16 19:45:52 +0100

    Responsivness for mobile and tablet

--- public/css/index.js
+++ public/css/index.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver ((entries) => {
+var io = new IntersectionObserver ((entries) => {
   entries.forEach((entry) => {
     console.log(entry)
     if (entry.isIntersecting) {
@@ -10,4 +10,20 @@ const observer = new IntersectionObserver ((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll('.hidden, .noblurhidden');
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach((el) => io.observe(el));
+
+
+
+var observer = new IntersectionObserver ((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('interaction');
+    } else {
+      entry.target.classList.remove('interaction');
+    }
+  });
+});
+
+const transElements = document.querySelectorAll('.title-img');
+transElements.forEach((el) => observer.observe(el));
